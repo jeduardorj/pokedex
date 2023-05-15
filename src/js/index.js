f863ebcb903b5f8e147079853e8a6483ea02dd5f
Eduardo Oliveira <zeduardo.aguiar@gmail.com>
2023-05-15 01:01:06 -0300

    Inclusão do JS que faz mudar a cor do background

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const bataoAlteraTema = document.getElementById("btn-alterar-tema")
+const body = document.querySelector('body')
+const imagemBtnTema = document.querySelector(".imagem-botao")
+
+bataoAlteraTema.addEventListener("click", () =>{
+    const modoEscuroAtivo = body.classList.contains("modo-escuro")
+    
+    body.classList.toggle("modo-escuro")
+
+    if(modoEscuroAtivo){   
+        imagemBtnTema.setAttribute("src", "/src/img/sun.png") 
+    }else{ 
+        imagemBtnTema.setAttribute("src", "/src/img/moon.png") 
+    }   
+    
+})
